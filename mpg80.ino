34952c6c39b9b004e67be1e76ccb667bca4f45c5
Victor Lomax <victorlomax@users.noreply.github.com>
2016-08-18 12:43:02 -0400

    mpg80.ino

---
+++ mpg80.ino
@@ -0,0 +1,11 @@
+#define MODIFIED 0x80
+
+#define _80PC 0x0332
+#define _75PC 0x02ff
+#define _66PC 0x02aa
+#define _60PC 0x0265
+#define _50PC 0x01ff
+#define _40PC 0x0198
+#define _33PC 0x0155
+#define _25PC 0x00ff
+#define _20PC 0x00cb
