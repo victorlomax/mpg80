c9b58a5f87c82dc481c1866db32fe347bf9657c9
Victor Lomax <victorlomax@users.noreply.github.com>
2017-03-09 16:35:40 -0500

    Update arpeggiator.h

--- sources/arpeggiator.h
+++ sources/arpeggiator.h
@@ -2,26 +2,32 @@
 
 // Arpeggiator statuses
 #define STOPPED 0
-#define STARTED 1
+#define STARTED 4
+
 // Arpeggiator modes
-#define UP      0
-#define DOWN    1
-#define UP&DOWN 2
-#define RND     3
+#define UP      16
+#define UPDOWN  20
+#define DOWN    8
+#define RND     32
 
 struct arpeggiator
 {
 private:
-  unsigned char _notes[16], // note pool
-                _pos,       // current position
-                _last;      // last note in the pool
-  unsigned char _status;    // current status: 0000mmms, mmm=mode, s=status
+  byte _notes[16], // note pool: 16 notes max
+       _octaves,   // Octaves
+       _tempo,     // Tempo
+       _pos,       // current position
+       _last,      // last note in the pool
+       _status;    // current status: [00mm msoo]
+                    // oo = octaves (0-3 = 1 to 4 octaves)
+                    // s = stopped (bit=0) or started (bit=1)
+                    // mmm = mode. 001=down, 010=up, 011=up&down, 1xx=random
 public:                
-  void add(unsigned char note),
-       del(unsigned char note),
-       play(unsigned int mode),
+  void add(byte note),
+       del(byte note),
+       play(byte mode, byte octaves, byte tempo), // Start the arpeggiator
        stop(),
        reset();
-  unsigned char next();
+  unsigned char play(void);     // Play the next note
   bool          status();
 };
