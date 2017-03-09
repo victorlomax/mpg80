aca7ad594599e6f2f2cdb83ecc76414c275826b1
Victor Lomax <victorlomax@users.noreply.github.com>
2017-03-09 18:15:49 -0500

    Update arpeggiator.cpp

--- sources/arpeggiator.cpp
+++ sources/arpeggiator.cpp
@@ -8,57 +8,38 @@
 // In µS : 62500 - 10417 @ 24 ppqn
 
 // Hold the arpeggio
-void arpeggiator::hold(byte channel, byte mode)
+void arpeggiator::hold(byte midi_channel, byte mode)
 {
+    if (channel[midi_channel&0x01]._status && HOLDON)
+        channel[midi_channel&0x01]._status &= !HOLDON;
+    else
+        channel[midi_channel&0x01]._status |= HOLDON;
 }
 
 // Add a note to the arpeggio
-void arpeggiator::add(byte channel, byte note)
+void arpeggiator::add(byte midi_channel, byte note)
 {
 }
 
 // Remove a note from the arpeggio
-void arpeggiator::del(byte channel, byte note)
+void arpeggiator::del(byte midi_channel, byte note)
 {
 }
 
 // Remove ALL notes from the arpeggio
-void arpeggiator::del(byte channel)
+void arpeggiator::del(byte midi_channel)
 {
 }
 
 // Play the arpeggio
-void arpeggiator::play(byte channel, word mode, word octaves, word)
+void arpeggiator::play(byte midi_channel, word mode, word octaves, word)
 {
     _status |= 0x01;
 }
 
 // Stop the arpeggiator
-void arpeggiator::stop()
+void arpeggiator::stop(byte midi_channel)
 {
     _status &= 0xfe;
     _pos = 0;
 }
-
-// Reset the arpeggio to the first note
-void arpeggiator::reset()
-{
-}
-
-// Reset the arpeggio to the first note
-void arpeggiator::reset()
-{
-}
-
-
-struct arpeggiator
-{
-private:
-  unsigned char _notes[16], // note pool
-                _pos,       // current position
-                _last;      // last note in the pool
-  unsigned char _status;    // current status: 0000mmms, mmm=mode, s=status
-       reset();
-  unsigned char next();
-  bool          status();
-};
