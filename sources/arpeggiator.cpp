ad35b11ad68caa5297f7fe9679e791232232cf0f
Victor Lomax <victorlomax@users.noreply.github.com>
2017-03-09 14:59:30 -0500

    Create arpeggiator.cpp

---
+++ sources/arpeggiator.cpp
@@ -0,0 +1,12 @@
+#ifndef _H_ARPEGGIATOR
+#include "arpeggiator.h"
+#endif
+
+// Arpeggiator range:
+// In bpm: 40 - 240 bpm
+// In mHz: 666.67 - 4000
+// In µS : 62500 - 10417 @ 24 ppqn
+
+void arpeggiator::reset()
+{
+}
