fff14c978cb07d0ad65833ef093fde52bd80cb65
Victor Lomax <victorlomax@users.noreply.github.com>
2017-03-09 15:38:12 -0500

    Update arpeggiator.cpp

--- sources/arpeggiator.cpp
+++ sources/arpeggiator.cpp
@@ -7,6 +7,48 @@
 // In mHz: 666.67 - 4000
 // In µS : 62500 - 10417 @ 24 ppqn
 
+// Add a note to the arpeggio
+void arpeggiator::add()
+{
+}
+
+// Remove a note from the arpeggio
+void arpeggiator::del()
+{
+}
+
+// Play the arpeggio
+void arpeggiator::play()
+{
+    _status |= 0x01;
+}
+
+// Stop the arpeggiator
+void arpeggiator::stop()
+{
+    _status &= 0xfe;
+    _pos = 0;
+}
+
+// Reset the arpeggio to the first note
 void arpeggiator::reset()
 {
 }
+
+// Reset the arpeggio to the first note
+void arpeggiator::reset()
+{
+}
+
+
+struct arpeggiator
+{
+private:
+  unsigned char _notes[16], // note pool
+                _pos,       // current position
+                _last;      // last note in the pool
+  unsigned char _status;    // current status: 0000mmms, mmm=mode, s=status
+       reset();
+  unsigned char next();
+  bool          status();
+};
