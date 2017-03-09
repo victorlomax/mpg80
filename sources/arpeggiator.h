d1378500114d1709bec6da1a6687103cd394f462
Victor Lomax <victorlomax@users.noreply.github.com>
2017-03-09 14:11:12 -0500

    Update arpeggiator.h

--- sources/arpeggiator.h
+++ sources/arpeggiator.h
@@ -1,6 +1,27 @@
 #define _H_ARPEGGIATOR
 
-typedef struct
+// Arpeggiator statuses
+#define STOPPED 0
+#define STARTED 1
+// Arpeggiator modes
+#define UP      0
+#define DOWN    1
+#define UP&DOWN 2
+#define RND     3
+
+struct arpeggio
 {
-  char  notes[16];
-} BUFFER;
+private:
+  unsigned char _notes[16], // note pool
+                _pos,       // current position
+                _last;      // last note in the pool
+  unsigned char _status;    // current status: 0000mmms, mmm=mode, s=status
+public:                
+  void add(unsigned char note),
+       del(unsigned char note),
+       play(unsigned int mode),
+       stop(),
+       reset();
+  unsigned char next();
+  bool          status();
+};
