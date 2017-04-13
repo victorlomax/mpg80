b46445c1d0653278bbcbab074481d44ef22c72ad
Victor Lomax <victorlomax@users.noreply.github.com>
2017-04-13 17:29:15 -0400

    Update midi.cpp

--- sources/midi.cpp
+++ sources/midi.cpp
@@ -3,24 +3,44 @@
 #include <midi.h>
 #endif
 
-char	midi_channel[2];
+byte	midi_channel[2];
 char events[MIDI_MAX];
 uchar	voice[48];
 PATCH	program[2];
 
+// This function decodes MIDI messages. 
+// We recognized only some messages; all other messages are sent to both MIDI OUT and MIDI THRU, hence not decoded.
+// MIDI messages are decoded byte per byte; the status is updated to keep track of what is we already received.
+// REALTIME MESSAGES:
+//      these messages can appear inside the MIDI flow; 
+// STATUS
+//  0x00: start
+//  0x01: MTC quarter frame message, 1 data byte expected
+//  0x02: SONG pointer position message, 2 data bytes expected
+//  0x03: Start OF Exclusive, several data bytes expected
+//  0x04: Timing Clock message, 
+//  0x05: Start message (no more data byte expected)
+//  0x06: Continue message (no more data byte expected)
+//  0x07: Stop message (no more data byte expected)
+//  0x08: NOTE OFF message begining, 2 data bytes expected
+//  0x09: NOTE ON message begining, 2 data bytes expected
+//  0x0b: CONTROL CHANGE message begining, 2 data bytes expected
+//  0x0c: PROGRAM CHANGE message begining, 2 data bytes expected
+//  0xDx: discard the next D bytes
+//  
 byte decode(byte b, byte status)
 {
     // These command bytes are ignored; we don't change the status
     switch(b)
     {
-      case 0xf3:  // Song Select
+      case 0xf3:  // Song Select, 2 DATA bytes following
       case 0xf4:  // not defined
       case 0xf5:  // not defined
       case 0xf6:  // Tune request
       case 0xf9:  // not defined
       case 0xfd:  // not defined
       case 0xfe:  // Active sensing 
-                return status;
+                return status;  // Discarded
     }
     switch(b & 0xf0)
     {
@@ -43,4 +63,10 @@ byte decode(byte b, byte status)
     }
     switch(b & 0xf0)    
     {
-      case 0x80: 
+      case 0x80:    // Note OFF
+      case 0x90:    // Note ON
+      case 0xb0:    // Control change
+      case 0xc0:    // Program change
+              if((b & 0x0f == midi_channel[0])||(b & 0x0f == midi_channel[1]))
+                return b & 0xf0;
+    }
