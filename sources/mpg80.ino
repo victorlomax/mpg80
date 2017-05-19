c962546d1f37a4b0851c5bbb1989ff5a26f7182f
Victor Lomax <victorlomax@users.noreply.github.com>
2017-05-19 16:34:47 -0400

    Add files via upload

--- sources/mpg80.ino
+++ sources/mpg80.ino
@@ -3,29 +3,29 @@
 #include <SPI.h>
 #include <MCP3008.h>
 
-// SC16IS750_SPI for a converter between SPI and a Serial port
-#include "mbed.h"
-#include "SC16IS752.h"
- 
-SPI spi(PTD2, PTD3, PTD1); //MOSI, MISO, SCK
-SC16IS752_SPI serial_bridge(&spi, PTD0);  // SPI port and CS pin
- 
-Serial pc(USBTX,USBRX);
- 
-int main() {
-  pc.printf("\nHello World!\n");
- 
-  serial_bridge.baud(9600);
-  //serial_bridge.printf("\nHello World!\n"); // supported through Stream
-  
-  while(1) { 
-    serial_bridge.ioSetState(0x00);
-    wait(0.5);
-    serial_bridge.ioSetState(0xFF); 
-    wait(0.5); 
-    serial_bridge.putc('*');  
-    pc.putc('*');                
-  }
+// SC16IS752_SPI for a converter between SPI and a Serial port
+#include "mbed.h"
+#include "SC16IS752.h"
+ 
+SPI spi(PTD2, PTD3, PTD1); //MOSI, MISO, SCK
+SC16IS752_SPI serial_bridge(&spi, PTD0);  // SPI port and CS pin
+ 
+Serial pc(USBTX,USBRX);
+ 
+int main() {
+  pc.printf("\nHello World!\n");
+ 
+  serial_bridge.baud(9600);
+  //serial_bridge.printf("\nHello World!\n"); // supported through Stream
+  
+  while(1) { 
+    serial_bridge.ioSetState(0x00);
+    wait(0.5);
+    serial_bridge.ioSetState(0xFF); 
+    wait(0.5); 
+    serial_bridge.putc('*');  
+    pc.putc('*');                
+  }
 }
 
 MCP button_pannel(1);
@@ -43,34 +43,55 @@ MCP button_pannel(1);
 
 
 // ***** SPI control *****
-// Port B (Pin 14-16) is used to select the channel (ie SPI device) on 74LS138
-#define SPICTL_DEVICE	PORTB
+// Port B0-2 (Pin 12-14) is used to select the channel (ie SPI device) on 74LS138
+// ARDUINO MINI PORT B
+//	Pin 12: port B bit 0
+//	Pin 13: port B bit 1
+//	Pin 14: port B bit 2
+//	Pin 15: port B bit 3 / SPI MOSI
+//	Pin 16: port B bit 4 / SPI MISO
+//	Pin 17: port B bit 5 / SPI SCK
+//	Pin  7: port B bit 6 / XTAL1
+//	Pin  8: port B bit 7 / XTAL2
 
-#define SPICTL_IDLE	0
-#define SPICTL_ADC		1
-#define SPICTL_BUTTON	2
-#define SPICTL_LED		3
-#define SPICTL_MIDI	4
-#define SPICTL_RAM		5
-#define	SPICTL_CS_PIN	12
-#define SPICTL_CLOCK_PIN   9
-#define SPICTL_MOSI_PIN    11
-#define SPICTL_MISO_PIN    10
+#define SPI_DEVICE_IDLE	0
+#define SPI_DEVICE_ADC		1
+#define SPI_DEVICE_BUTTON	2
+#define SPI_DEVICE_LED		3
+#define SPI_DEVICE_MIDI	4
+#define SPI_DEVICE_RAM		5
+#define	SPI_CS_PIN	12
+#define SPI_CLOCK_PIN   9
+#define SPI_MOSI_PIN    11
+#define SPI_MISO_PIN    10
 
 char values[28];
 
+void spi_select(byte value)
+{
+	PORTB &= 0xf8;
+	if(value==SPI_DEVICE_IDLE)
+	{
+		SPI.endTransaction();
+		digitalwrite(SPI_SELECT, HIGH);
+	} else {
+		PORTB |= (value & 0x05);
+		SPI.beginTransaction();
+		digitalwrite(SPI_SELECT, LOW);
+	}
+}
+
 void setup()
 {
+	// Port B (Pin 14-16) is used to select the channel (ie SPI device) on 74LS138
+	DDRB |= 0x05;	// Set pins 14-16 of Port B as OUTPUT
 	...
 	SPI.begin();
 
-	pinMode(SPI_SELECT, OUTPUT);
 
 	pinMode(23, OUTPUT);	// Port C.0
 	pinMode(24, OUTPUT);	// Port C.1
 
-	pinMode(14, OUTPUT);	// Port B.0
-	pinMode(15, OUTPUT);	// Port B.1
 						
 	...
 }
@@ -83,8 +104,14 @@ void pot_scan(char *values)
 	int board, channel, value;
 	
 	
-    serial_bridge.ioSetState(0x00);    wait(0.5);    serial_bridge.ioSetState(0xFF);     wait(0.5);     serial_bridge.putc('*');      pc.putc('*');                
-	digitalwrite(SPI_DEVICE, SPI_ADC);
+	serial_bridge.ioSetState(0x00);
+	wait(0.5);
+	serial_bridge.ioSetState(0xFF);
+	wait(0.5);
+	serial_bridge.putc('*');
+	pc.putc('*');
+
+	spi_select(SPI_DEVICE_ADC);
 	digitalwrite(SPI_SELECT, LOW);
 	SPI.beginTransaction(SPISettings(3600000, MSBFIRST, SPI_MODE0))
 
@@ -102,3 +129,5 @@ void pot_scan(char *values)
 	SPI.endTransaction();
 	digitalwrite(SPI_SELECT, HIGH);
 }
+
+
